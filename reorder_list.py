7fd4c38018976a9a27bcba0656820b2f67bdc9eb
flybywind <flybywind@foxmail.com>
2014-06-27 13:28:24 +0800

    add reorder list

---
+++ reorder_list.py
@@ -0,0 +1,40 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    # @param head, a ListNode
+    # @return nothing
+    def reorderList(self, head):
+        if head is None or head.next is None or head.next.next is None:
+            return head
+
+        mid = last = head
+        while last is not None:
+            last = last.next
+            if last is not None:
+                last = last.next
+            else:
+                break
+            mid = mid.next
+        prev = mid
+        mid = mid.next
+        prev.next = None
+        prev = None
+        while mid is not None:
+            next = mid.next
+            mid.next = prev
+            prev = mid
+            mid = next
+        j = prev
+        i = head
+        while i is not None and j is not None:
+            n1 = i.next
+            n2 = j.next
+            i.next = j
+            j.next = n1
+            i = n1
+            j = n2        
+        return head
