567f506e873a69c5a54133f3cc391e2609ed4897
tongxin.wen <flybywind@foxmail.com>
2023-07-16 11:01:22 +0800

    fixing some edge cases

--- golang/atMostNGivenDigitSet/main.go
+++ golang/atMostNGivenDigitSet/main.go
@@ -1,5 +1,59 @@
 package atmostngivendigitset
 
-func atMostNGivenDigitSet(digits []string, n int) int {
+import (
+	"math"
+	"sort"
+	"strconv"
+)
 
+func getDecBits(n int) []string {
+	ret := make([]string, 0, int(math.Log10(float64(n))+1))
+	for n > 0 {
+		ret = append(ret, strconv.FormatInt(int64(n%10), 10))
+		n = n / 10
+	}
+	return ret
+}
+func numOflessThan(digits []string, v string) int {
+	return sort.SearchStrings(digits, v)
+}
+func atMostNGivenDigitSet(digits []string, n int) int {
+	dn := len(digits)
+	sum := 0
+	m := 1
+	decbit := getDecBits(n)
+	multCache := make([]int, 0, len(decbit)+1)
+	multCache = append(multCache, 1)
+	for i := 0; i < len(decbit)-1; i++ {
+		m = m * dn
+		multCache = append(multCache, m)
+		sum += m
+	}
+
+	lastNum := ""
+	for i := len(decbit) - 1; i >= 0; i-- {
+		// case ["1"], 856
+		if i > 0 && lastNum != "" && lastNum < decbit[i-1] {
+			break
+		}
+		lessnum := numOflessThan(digits, decbit[i])
+		if i == 0 && lessnum < len(digits) &&
+			digits[lessnum] == decbit[i] {
+			lessnum++
+		}
+		if lessnum < 1 { // case [ "1", "3", "4"] 125
+			if digits[lessnum] == decbit[i] {
+				continue
+			} else {
+				break
+			}
+		}
+		sum += lessnum * multCache[i]
+		if lessnum < len(digits) {
+			lastNum = digits[lessnum]
+		} else {
+			lastNum = digits[len(digits)-1]
+		}
+	}
+	return sum
 }
--- golang/atMostNGivenDigitSet/main_test.go
+++ golang/atMostNGivenDigitSet/main_test.go
@@ -1,7 +1,31 @@
 package atmostngivendigitset
 
-import "testing"
+import (
+	"testing"
 
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCase(t *testing.T) {
+	assert.Equal(t, 1, atMostNGivenDigitSet([]string{"7"}, 8))
+	assert.Equal(t, 2, atMostNGivenDigitSet([]string{"1", "3", "5", "7"}, 3))
+}
 func TestCase1(t *testing.T) {
+	assert.Equal(t, 20, atMostNGivenDigitSet([]string{"1", "3", "5", "7"}, 100))
+}
 
+func TestCase2(t *testing.T) {
+	assert.Equal(t, 29523, atMostNGivenDigitSet([]string{"1", "4", "9"}, 1000000000))
+}
+
+func TestCase3(t *testing.T) {
+	assert.Equal(t, 47, atMostNGivenDigitSet([]string{"1", "3", "5", "7"}, 256))
+}
+
+func TestCase4(t *testing.T) {
+	assert.Equal(t, 6, atMostNGivenDigitSet([]string{"5", "7", "8"}, 59))
+}
+
+func TestCase5(t *testing.T) {
+	assert.Equal(t, 3, atMostNGivenDigitSet([]string{"1"}, 834))
 }
