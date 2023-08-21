79e35cf84002769093fe156a208eed8af310281d
tongxin.wen <flybywind@foxmail.com>
2023-08-21 21:25:39 +0800

    init

---
+++ java_proj/leetcode/src/main/java/prac/leetcode/StringVariances.java
@@ -0,0 +1,48 @@
+package prac.leetcode;
+/*
+'A' -> "1"
+'B' -> "2"
+…
+'J' -> “10”
+'K' -> “11”
+'Z' -> "26"
+
+Example:
+# 1      ->    1 (A)
+# 12    ->    2 (AB,     L)
+# 123  ->    3 (ABC , LC, AW )
+
+input: 0-9 string
+output: a number
+ */
+public class StringVariances {
+    static public int parseNum(String input) {
+        int n = input.length();
+        if (n <= 1) {
+            if (input.equals("0")) {
+                return 0;
+            }
+            return n;
+        }
+        int pos_num = 0;
+        boolean findInvaid = false;
+        for (int i=1; i < n+1; ++i) {
+            int ind = Integer.parseInt(input.substring(0, i));
+            if (ind > 0 && ind <=26) {
+                int r = parseNum(input.substring(i, n));
+                if (r >= 0) {
+                    pos_num += r;
+                }
+            else {
+                    findInvaid = true;
+                    break;
+                }
+            }
+        }
+        if (findInvaid && pos_num == 0) {
+            return 0;
+        }
+
+        return pos_num;
+    }
+}
---
+++ java_proj/leetcode/src/test/java/prac/leetcode/StringVariancesTest.java
@@ -0,0 +1,12 @@
+package prac.leetcode;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StringVariancesTest {
+
+    @org.junit.jupiter.api.Test
+    void parseNum() {
+        String input = "123";
+        assertEquals(3, StringVariances.parseNum(input));
+    }
+}
