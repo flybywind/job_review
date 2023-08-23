f427e3a9de250a4acf130109810b64a8099037a6
tongxin.wen <flybywind@foxmail.com>
2023-08-23 16:46:42 +0800

    rename effortless search path

--- scala_proj/leetcode_sc3/src/main/scala/sc3/leetcode/minEffortPath.scala
+++ scala_proj/leetcode_sc3/src/main/scala/sc3/leetcode/minEffortPath.scala
@@ -9,10 +9,10 @@ object minEffortPath {
   private case class NodeEff(loc:(Int, Int), effort: Int)
 
   private def orderNode(x:NodeEff) = -x.effort
-  def minEffortPath(heights: Array[Array[Int]]): Int =
-    var visited = Set[(Int, Int)]()
+  def minimumEffortPath(heights: Array[Array[Int]]): Int =
     val rowNum = heights.size
     val colNum = heights(0).size
+    val visited = 0.until(rowNum).map(x=>Array.ofDim[Int](colNum)).toArray
 
     var nodeeff = NodeEff((0, 0), 0)
     val heap = new mutable.PriorityQueue[NodeEff]()(Ordering.by(orderNode))
@@ -25,11 +25,12 @@ object minEffortPath {
         if minEff > head.effort then
           minEff = head.effort
       else
-        visited = visited + head.loc
+        visited(i)(j) = 1
         for d <- Array((1, 0), (-1, 0), (0, 1), (0, -1)) do
           val (i2, j2) = (d._1+i, d._2+j)
           if (i2 >= 0 && i2 < rowNum) &&
-            (j2 >= 0 && j2 < colNum) && !visited((i2, j2)) then
+             (j2 >= 0 && j2 < colNum) &&
+            visited(i2)(j2) == 0 then
             nodeeff = NodeEff((i2, j2), math.max(head.effort, math.abs(heights(i)(j) - heights(i2)(j2))))
             heap.enqueue(nodeeff)
 
--- scala_proj/leetcode_sc3/src/test/scala/sc3/leetcode/minEffortPathTest.scala
+++ scala_proj/leetcode_sc3/src/test/scala/sc3/leetcode/minEffortPathTest.scala
@@ -1,14 +1,14 @@
 package sc3.leetcode
 
 import org.scalatest.flatspec.AnyFlatSpec
-import minEffortPath.minEffortPath
+import minEffortPath.minimumEffortPath
 class minEffortPathTest extends AnyFlatSpec {
 
   behavior of "minEffortPathTest"
 
   it should "minEffortPath" in {
     assertResult(1) {
-      minEffortPath(Array[Array[Int]](
+      minimumEffortPath(Array[Array[Int]](
         Array[Int](1,2,3),
         Array[Int](3,8,4),
         Array[Int](5,3,5)
@@ -16,7 +16,7 @@ class minEffortPathTest extends AnyFlatSpec {
     }
 
     assertResult(2) {
-      minEffortPath(Array[Array[Int]](
+      minimumEffortPath(Array[Array[Int]](
         Array[Int](1, 2, 2),
         Array[Int](3, 8, 2),
         Array[Int](5, 3, 5)
@@ -24,7 +24,7 @@ class minEffortPathTest extends AnyFlatSpec {
     }
 
     assertResult(0) {
-      minEffortPath(Array[Array[Int]](
+      minimumEffortPath(Array[Array[Int]](
         Array[Int](1,2,1,1,1),
         Array[Int](1,2,1,2,1),
         Array[Int](1,2,1,2,1),
