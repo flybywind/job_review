6ba2104d204b9c6b7a48cd7aa2be5e34d3199fbf
tongxin.wen <flybywind@foxmail.com>
2023-08-25 19:37:20 +0800

    phase test

--- scala_proj/leetcode_sc3/src/main/scala/pat/consumerproducer/service.scala
+++ scala_proj/leetcode_sc3/src/main/scala/pat/consumerproducer/service.scala
@@ -1,7 +1,7 @@
 package pat.consumerproducer
 
 import java.util.concurrent.atomic.{AtomicInteger, AtomicLong}
-import java.util.concurrent.{ArrayBlockingQueue, CountDownLatch, Semaphore}
+import java.util.concurrent.{ArrayBlockingQueue, CountDownLatch, Phaser, Semaphore}
 import scala.concurrent.{Await, Future}
 import concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration.{Duration, SECONDS}
@@ -17,26 +17,27 @@ object service {
   @main def main() =
     val consumerCnt = 3
     val procCnt = AtomicInteger(0)
-    val countDown = CountDownLatch(1)
+    val phaser = Phaser(1)
 
     def genPerson(): Person =
       Person(namelist(Random.nextInt(namelist.size)), 18 + Random.nextInt(40))
 
     def procPerson(p: Person) =
       val totalProc = procCnt.incrementAndGet()
-      if totalProc > consumerCnt*100 then countDown.countDown()
       println(f"consume person: $p")
+      if totalProc > consumerCnt*100 then phaser.arrive()
 
     val blockQueue = ArrayBlockingQueue[Person](10)
     val producer = Producer[Person](blockQueue, gen = genPerson)
 
     var futSeq = producer.start :: Nil
-    for i <- 0 until 3 do
+    for i <- 0 until consumerCnt do
+      phaser.register()
       val consumer = Consumer[Person](blockQueue, proc = procPerson)
       futSeq = consumer.consume :: futSeq
 
 //    Await.result(Future.sequence(futSeq), Duration(5, SECONDS))
 //    Thread.sleep(5000)
-    countDown.await()
+    phaser.arriveAndAwaitAdvance()
     println("leave main")
 }
