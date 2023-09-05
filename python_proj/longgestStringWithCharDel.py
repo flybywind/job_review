e87c3a2cfe8af16dc1673a66a3b7b910da1cb1ee
tongxin.wen <flybywind@foxmail.com>
2023-09-05 16:38:27 +0800

    optimize by 30x with dict first approach

--- python_proj/longgestStringWithCharDel.py
+++ python_proj/longgestStringWithCharDel.py
@@ -1,22 +1,5 @@
 from typing import List
 from collections import defaultdict
-import heapq
-
-
-class HeapNode:
-    def __init__(self, prefix, p) -> None:
-        self.prefix = prefix
-        self.potential = 0
-        self.currentPos = p
-
-    def __lt__(self, other) -> bool:
-        if self.potential != other.potential:
-            return self.potential > other.potential
-        else:
-            return self.prefix < other.prefix
-
-    def __repr__(self) -> str:
-        return f"{self.prefix}@{self.currentPos}, potential = {self.potential}"
 
 
 class Solution:
@@ -24,56 +7,36 @@ class Solution:
     #     self.mapTrie = set()
     #     self.wholeWord = set()
     def findLongestWord(self, s: str, dictionary: List[str]) -> str:
-        mapTrie = defaultdict(int)  # prefix and its potential longgest word
-        wholeWord = set(dictionary)
-        for word in dictionary:
-            wn = len(word)
-            for i in range(1, len(word)+1):
-                if mapTrie[word[:i]] < wn:
-                    mapTrie[word[:i]] = wn
-        strN = len(s)
-        occurPos = [strN]*26
+        queryPos = defaultdict(list)
         for p, b in enumerate(s):
-            i = ord(b) - ord('a')
-            if occurPos[i] > p:
-                occurPos[i] = p
+            queryPos[b].append(p)
+        dictionary = sorted(dictionary, key=lambda s: (-len(s), s))
 
-        heapAry = []
-        for i, p in enumerate(occurPos):
-            hn = HeapNode('', p)
-            hn.potential = strN - p
-            if hn.potential > 0:
-                heapAry.append(hn)
+        for d in dictionary:
+            dictPos = defaultdict(int)
+            for b in d:
+                dictPos[b] += 1
 
-        heapq.heapify(heapAry)
-        ans = ''
-        visited = set()
-        while len(heapAry) > 0:
-            hn: HeapNode = heapq.heappop(heapAry)
-            # print(f"<<| pop heap: {hn}")
-            for i in range(hn.currentPos, strN):
-                prefix2 = hn.prefix + s[i]
-                if prefix2 in visited:
-                    # print(f"skip, {prefix2} has visited")
-                    continue
-                visited.add(prefix2)
-                if prefix2 in mapTrie:
-                    if prefix2 in wholeWord:
-                        if len(prefix2) > len(ans) or \
-                                (len(prefix2) == len(ans) and prefix2 < ans):
-                            ans = prefix2
-                            # print(f"find ans: {ans}")
-                            heapAry = [
-                                hn for hn in heapAry if hn.potential >= len(ans)]
-                            heapq.heapify(heapAry)
+            lenCover = True
+            for b, cnt in dictPos.items():
+                if cnt > len(queryPos[b]):
+                    lenCover = False
+                    break
+            if not lenCover:
+                continue
 
-                    potential = min(
-                        strN - i - 1 + len(prefix2), mapTrie[prefix2])
-                    if potential > len(ans) or \
-                            (potential == len(ans) and ans[:len(prefix2)] > prefix2):
-                        hn2 = HeapNode(prefix=prefix2, p=i+1)
-                        hn2.potential = potential
-                        # print(f">> push heap: {hn2}")
-                        heapq.heappush(heapAry, hn2)
-
-        return ans
+            lastPos = -1
+            found = True
+            for b in d:
+                posList = queryPos[b]
+                newPos = lastPos
+                for p in posList:
+                    if p > lastPos:
+                        newPos = p
+                        break
+                if newPos == lastPos:
+                    found = False
+                    break
+                lastPos = newPos
+            if found:
+                return d
