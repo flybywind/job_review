1275729bab22989a0f44579044b70e1d8a6068c8
tongxin.wen <flybywind@foxmail.com>
2023-09-05 15:52:06 +0800

    optimize with heap

--- python_proj/longgestStringWithCharDel.py
+++ python_proj/longgestStringWithCharDel.py
@@ -1,4 +1,19 @@
 from typing import List
+from collections import defaultdict
+import heapq
+
+
+class HeapNode:
+    def __init__(self, prefix, p) -> None:
+        self.prefix = prefix
+        self.potential = 0
+        self.currentPos = p
+
+    def __lt__(self, other) -> bool:
+        return self.potential > other.potential
+
+    def __repr__(self) -> str:
+        return f"{self.prefix}@{self.currentPos}, potential = {self.potential}"
 
 
 class Solution:
@@ -6,38 +21,53 @@ class Solution:
     #     self.mapTrie = set()
     #     self.wholeWord = set()
     def findLongestWord(self, s: str, dictionary: List[str]) -> str:
-        mapTrie = set()
+        mapTrie = defaultdict(int)  # prefix and its potential longgest word
         wholeWord = set(dictionary)
         for word in dictionary:
+            wn = len(word)
             for i in range(1, len(word)+1):
-                mapTrie.add(word[:i])
+                if mapTrie[word[:i]] < wn:
+                    mapTrie[word[:i]] = wn
+        strN = len(s)
+        occurPos = [strN]*26
+        for p, b in enumerate(s):
+            i = ord(b) - ord('a')
+            if occurPos[i] > p:
+                occurPos[i] = p
 
-        ans = ""
-        itered = set()
+        heapAry = []
+        for i, p in enumerate(occurPos):
+            hn = HeapNode('', p)
+            hn.potential = strN - p
+            if hn.potential > 0:
+                heapAry.append(hn)
 
-        def _recurs(s, strN, pre, i):
-            nonlocal ans
-            if i >= strN:
-                return
-            if pre in itered:
-                return
-            itered.add(pre)
-            for j in range(i, strN):
-                potentialLen = (strN - j) + len(pre)
-                if potentialLen < len(ans):
-                    return
+        heapq.heapify(heapAry)
+        ans = ''
+        visited = set()
+        while len(heapAry) > 0:
+            hn: HeapNode = heapq.heappop(heapAry)
+            # print(f"<<| pop heap: {hn}")
+            for i in range(hn.currentPos, strN):
+                prefix2 = hn.prefix + s[i]
+                if prefix2 in visited:
+                    # print(f"skip, {prefix2} has visited")
+                    continue
+                visited.add(prefix2)
+                if prefix2 in wholeWord:
+                    if len(prefix2) > len(ans) or \
+                            (len(prefix2) == len(ans) and prefix2 < ans):
+                        ans = prefix2
+                        # print(f"find ans: {ans}")
+                        heapAry = [
+                            hn for hn in heapAry if hn.potential >= len(ans)]
+                        heapq.heapify(heapAry)
 
-                pre2 = pre + s[j]
-                # print(f"ans = {ans}, iter {pre2} at {j}")
+                hn2 = HeapNode(prefix=prefix2, p=i+1)
+                hn2.potential = min(
+                    strN - i - 1 + len(hn2.prefix), mapTrie[hn2.prefix])
+                if hn2.potential >= len(ans):
+                    # print(f">> push heap: {hn2}")
+                    heapq.heappush(heapAry, hn2)
 
-                if pre2 in mapTrie:
-                    # print(f"search {pre2} from {j+1}")
-                    if pre2 in wholeWord:
-                        if len(pre2) > len(ans):
-                            ans = pre2
-                        elif len(pre2) == len(ans) and pre2 < ans:
-                            ans = pre2
-                    _recurs(s, strN, pre2, j+1)
-
-        _recurs(s, len(s), '', 0)
         return ans
