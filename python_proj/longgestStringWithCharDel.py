35f93fbb6cf58d0e57688293a4124f18e14607f8
tongxin.wen <flybywind@foxmail.com>
2023-09-05 13:31:53 +0800

    optimize longgestStringWithCharDel

--- python_proj/longgestStringWithCharDel.py
+++ python_proj/longgestStringWithCharDel.py
@@ -13,25 +13,34 @@ class Solution:
                 mapTrie.add(word[:i])
 
         ans = ""
+        pruneCache = set()
 
         def _recurs(s, strN, pre, i):
-            nonlocal ans
+            nonlocal ans, pruneCache
             if i == strN:
                 return
-            for j in range(i, strN):
-                potentialLen = (strN - j) + len(pre)
-                if potentialLen < len(ans):
-                    break
-                pre2 = pre + s[j]
+            potentialLen = (strN - i - 1) + len(pre)
+            if potentialLen < len(ans):
+                return
+
+            key = pre + str(i+1)
+            if key not in pruneCache:
+                # print(f"search {pre} from {i+1}, skip {i}")
+                pruneCache.add(key)
+                _recurs(s, strN, pre, i+1)
+
+            pre2 = pre + s[i]
+            key = pre2 + str(i+1)
+            if key not in pruneCache:
+                pruneCache.add(key)
                 if pre2 in mapTrie:
+                    # print(f"search {pre2} from {i+1}")
                     if pre2 in wholeWord:
                         if len(pre2) > len(ans):
                             ans = pre2
                         elif len(pre2) == len(ans) and pre2 < ans:
                             ans = pre2
-                    _recurs(s, strN, pre2, j+1)
-
-            return
+                    _recurs(s, strN, pre2, i+1)
 
         _recurs(s, len(s), '', 0)
         return ans
