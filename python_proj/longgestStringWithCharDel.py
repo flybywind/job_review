77d3b4bb3009823ab5d9987ea489156b1c5853e4
tongxin.wen <flybywind@foxmail.com>
2023-09-05 17:21:15 +0800

    done with longgest word with char removed

--- python_proj/longgestStringWithCharDel.py
+++ python_proj/longgestStringWithCharDel.py
@@ -1,11 +1,81 @@
 from typing import List
 from collections import defaultdict
 
+# https://leetcode.cn/problems/longest-word-in-dictionary-through-deleting/solutions/996014/tong-guo-shan-chu-zi-mu-pi-pei-dao-zi-di-at66/ 双指针
+
+"""
+# 尝试了静态数组代替defaultdict，发现并不能减少内存使用
+
+from typing import List
+from collections import defaultdict
+
 
 class Solution:
-    # def __init__(self) -> None:
-    #     self.mapTrie = set()
-    #     self.wholeWord = set()
+    def __init__(self) -> None:
+        self.queryPos = []
+        self.dictPos = [0]*26
+        self.queryPosCnt = [0]*26
+        for _ in range(26):
+            self.queryPos.append([0]*10)
+
+    def cleanQueryPos(self):
+        self.queryPosCnt = [0]*26
+
+    def appendQueryPos(self, idx, p):
+        if len(self.queryPos[idx]) == self.queryPosCnt[idx]:
+            self.queryPos[idx] += [0]*(1+self.queryPosCnt[idx])
+        self.queryPos[idx][self.queryPosCnt[idx]] = p
+        self.queryPosCnt[idx] += 1
+
+    def cleanDictPos(self):
+        self.dictPos = [0]*26
+
+    def findLongestWord(self, s: str, dictionary: List[str]) -> str:
+        self.cleanQueryPos()
+        for p, b in enumerate(s):
+            idx = ord(b) - ord('a')
+            self.appendQueryPos(idx, p)
+
+        dictionary = sorted(dictionary, key=lambda s: (-len(s), s))
+
+        for d in dictionary:
+            self.cleanDictPos()
+            for b in d:
+                self.dictPos[ord(b) - ord('a')] += 1
+
+            lenCover = True
+            for b in d:
+                idx = ord(b)-ord('a')
+                cnt = self.dictPos[idx]
+                if cnt > self.queryPosCnt[idx]:
+                    lenCover = False
+                    break
+            if not lenCover:
+                continue
+
+            lastPos = -1
+            found = True
+            for b in d:
+                idx = ord(b)-ord('a')
+                posList = self.queryPos[idx][:self.queryPosCnt[idx]]
+                newPos = lastPos
+                for p in posList:
+                    if p > lastPos:
+                        newPos = p
+                        break
+                if newPos == lastPos:
+                    found = False
+                    break
+                lastPos = newPos
+            if found:
+                return d
+        return ''
+
+"""
+
+
+class Solution:
+
     def findLongestWord(self, s: str, dictionary: List[str]) -> str:
         queryPos = defaultdict(list)
         for p, b in enumerate(s):
